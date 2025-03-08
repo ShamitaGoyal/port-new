c3899318c24020c927826b0825db47ec2c58d2ee
Shamita <goyal.shamita4@gmail.com>
2025-03-07 21:15:05 -0800

    added the for fun section a little bit

--- src/Pages/ForFun/Forfun.jsx
+++ src/Pages/ForFun/Forfun.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../ForFun/forfun.css'
+import projects from './forfunData'; 
 
 
 function Forfun() {
@@ -10,6 +11,33 @@ function Forfun() {
         <h1>For Fun</h1>
         <h5>To show some things I am passionate about!</h5>
       </div>
+
+      <div className="art-title">
+        <h5>Selected artworks: </h5>
+      </div>
+
+      <div className="card-container">
+              {projects.map((project, index) => (
+              <div key={index} className="col-md-4">
+                  <div className="card">
+                  <a href={project.link} target="_blank" rel="noopener noreferrer">
+                      <img src={project.image} className="card-img" alt={project.title} />
+                  </a>
+                  <div className="card-text">
+                      <h5 className="card-name-small">{project.role}</h5>
+                      {/* <p className="card-name-small">{project.tools}</p> */}
+                      <p className="card-name-large">{project.title}</p>
+                      <h5 className="card-name-small-para">{project.description}</h5>
+                  </div>
+                  </div>
+              </div>
+              ))}
+      </div>
+
+
+
+
+      
     </div>
   )
 }
